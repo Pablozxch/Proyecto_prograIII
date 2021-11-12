7150978fa085816649ea20c3a9892bef543925fb
Pablozxch <jp0151@hotmail.com>
2021-11-12 11:14:11 -0600

    GG

--- ProyectoRestaurante/src/main/resources/cr/ac/una/proyectorestaurante/controllers/FacturaController.java
+++ ProyectoRestaurante/src/main/resources/cr/ac/una/proyectorestaurante/controllers/FacturaController.java
@@ -8,6 +8,7 @@ package cr.ac.una.proyectorestaurante.controllers;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXToggleButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -44,17 +45,19 @@ public class FacturaController extends Controller implements Initializable {
     @FXML
     private JFXTextField txtDireccionRest;
     @FXML
-    private JFXCheckBox chkEfectivo;
-    @FXML
-    private JFXCheckBox chkTarjeta;
-    @FXML
     private JFXTextField txtImpuestos;
-    @FXML
-    private JFXButton btnGuardar;
     @FXML
     private JFXButton btnBuscarCodDescuento;
     @FXML
     private JFXButton btnPagar;
+    @FXML
+    private JFXToggleButton chkTarjetaEfectivo;
+    @FXML
+    private JFXTextField txtMontoAPagar;
+    @FXML
+    private JFXTextField txtPagaCon;
+    @FXML
+    private JFXButton btnEnviarCorreo;
     /**
      * Initializes the controller class.
      */
